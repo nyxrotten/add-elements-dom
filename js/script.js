06b4497630e618692d3db94e6cf6ace0a165af52
nyxrotten <nes_calina@hotmail.com>
2024-02-21 17:23:14 +0100

    terminado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+
+
 agregar.addEventListener('click', function() {
     let elementoLista = prompt('Añade un elemento a la lista');
     let nuevoLi = document.createElement('li');
     document.getElementById('lista').appendChild(nuevoLi);
-    nuevoLi.appendChild(elementoLista);
     nuevoLi.insertAdjacentHTML('afterbegin', elementoLista);
 });
 
